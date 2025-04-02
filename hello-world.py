7980417477f6a641a0aeaecd1f940eb47d47271e
Evelyn Tran <evelyntran224@gmail.com>
2025-04-01 19:27:33 -0500

    Implemented Hello World Program

---
+++ hello-world.py
@@ -0,0 +1,38 @@
+def helloWorld():
+    for i in len("Hello World!"):
+        if i == 0:
+            print("H")
+            continue
+        elif i == 1:
+            print("e")
+            continue
+        elif i == 2:
+            print("l")
+            continue
+        elif i == 3:
+            print("l")
+            continue
+        elif i == 4:
+            print("o")
+            continue
+        elif i == 5:
+            print(" ")
+            continue
+        elif i == 6:
+            print("W")
+            continue
+        elif i == 7:
+            print("o")
+            continue
+        elif i == 8:
+            print("r")
+            continue
+        elif i == 9:
+            print("l")
+            continue
+        elif i == 10:
+            print("d")
+            continue
+        elif i == 11:
+            print("!")
+        
