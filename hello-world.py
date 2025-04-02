33157664ab8ea20bb1c98b5e99e2ea53a689cc7c
Evelyn Tran <evelyntran224@gmail.com>
2025-04-01 19:38:22 -0500

    Fixed errors

--- hello-world.py
+++ hello-world.py
@@ -1,38 +1,38 @@
 def helloWorld():
-    for i in len("Hello World!"):
+    for i in range(len("Hello World!")):
         if i == 0:
-            print("H")
+            print("H", end = "")
             continue
         elif i == 1:
-            print("e")
+            print("e", end = "")
             continue
         elif i == 2:
-            print("l")
+            print("l", end = "")
             continue
         elif i == 3:
-            print("l")
+            print("l", end = "")
             continue
         elif i == 4:
-            print("o")
+            print("o", end = "")
             continue
         elif i == 5:
-            print(" ")
+            print(" ", end = "")
             continue
         elif i == 6:
-            print("W")
+            print("W", end = "")
             continue
         elif i == 7:
-            print("o")
+            print("o", end = "")
             continue
         elif i == 8:
-            print("r")
+            print("r", end = "")
             continue
         elif i == 9:
-            print("l")
+            print("l", end = "")
             continue
         elif i == 10:
-            print("d")
+            print("d", end = "")
             continue
         elif i == 11:
-            print("!")
-        
+            print("!", end = "")
+helloWorld()
